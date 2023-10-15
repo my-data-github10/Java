8aa5623e61b2f6567e76db9ecd444a031b2a5bdb
jagdish-10 <jagdishchavan2@gmail.com>
2023-10-15 15:14:54 +0530

    Assignment2 Uploaded

---
+++ Assignment2/Assignment2/src/com/appinfo/invoice/TestInvoice.java
@@ -0,0 +1,12 @@
+package com.appinfo.invoice;
+
+public class TestInvoice {
+
+	public static void main(String[] args) {
+		Invoice i1=new Invoice();
+		i1.accept();
+		i1.display();
+		i1.calInvoice();
+
+	}
+}
---
+++ Assignment2/Assignment2/src/question3/DateTest.java
@@ -0,0 +1,15 @@
+package question3;
+
+public class DateTest {
+
+	public static void main(String[] args) {
+		
+		Date d = new Date();
+		d.setDay(5);
+		d.setMonth(10);
+		d.setYear(2000);
+		d.displayDate();
+
+	}
+
+}
