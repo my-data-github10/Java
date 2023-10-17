64c17a79220662ef0b213559bcb7f522271c40f7
jagdish-10 <jagdishchavan2@gmail.com>
2023-10-17 21:59:40 +0530

    Assignment6 uploaded

---
+++ Assignment6/Question1/src/com/sunbeam/ExceptionLineTooLong.java
@@ -0,0 +1,27 @@
+package com.sunbeam;
+
+public class ExceptionLineTooLong extends RuntimeException
+{
+	private String exceptionMsg;
+	
+	public ExceptionLineTooLong() 
+	{
+		
+	}
+
+	public ExceptionLineTooLong(String exceptionMsg) 
+	{	
+		this.exceptionMsg = exceptionMsg;
+	}
+	public void displayException()
+	{
+		System.out.println(this.exceptionMsg);
+	}
+
+	@Override
+	public String toString() {
+		return "ExceptionLineTooLong= " + exceptionMsg;
+	}
+	
+
+}
